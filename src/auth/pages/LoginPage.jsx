77a8a21861752797ccb59d13584cb9a9dd51329d
Cristian1805 <cristiancamilolozanogomez@gmail.com>
2023-10-17 12:41:35 -0500

    Formato responsivo añadido al formulario

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,13 +1,11 @@
-import React from 'react'
+import React from 'react';
 
-// import React from 'react';
-
-export const LoginPage = () => { 
+export const LoginPage = () => {
   return (
     <div className="container h-100">
       <div className="row h-100 justify-content-center align-items-center">
         {/* Columna para el logo de la empresa */}
-        <div className="col-md-6 text-center">
+        <div className="col-md-6 col-sm-12 text-center">
           <img
             src="/assets/logo.jpg"
             alt="Logo de la Empresa"
@@ -16,15 +14,15 @@ export const LoginPage = () => {
           <h3 className="display-6">Bienvenido al inventario</h3>
         </div>
         {/* Columna para el formulario de inicio de sesión */}
-        <div className="col-md-6">
+        <div className="col-md-6 col-sm-12">
           <div className="mt-3 text-center">
-          <h3 className="display-6">Formulario Inicio de sesión</h3>
-          <hr className="w-50 mx-auto mb-4" />
+            <h3 className="display-6">Formulario Inicio de sesión</h3>
+            <hr className="w-50 mx-auto mb-4" />
           </div>
           <form>
             <div className="form-group">
               <label htmlFor="usuario">Usuario:</label>
-              <input 
+              <input
                 type="text"
                 className="form-control"
                 id="usuario"
@@ -32,18 +30,18 @@ export const LoginPage = () => {
               />
             </div>
             <div className="form-group mb-3">
-            <label htmlFor="contrasena">Contraseña:</label> 
-            <input
+              <label htmlFor="contrasena">Contraseña:</label>
+              <input
                 type="password"
                 className="form-control"
                 id="contrasena"
                 placeholder="Contraseña"
-            />
+              />
             </div>
             <div className="form-group">
-            <button type="submit" className="btn btn-primary btn-block">
+              <button type="submit" className="btn btn-primary">
                 Iniciar Sesión
-            </button>
+              </button>
             </div>
           </form>
         </div>
@@ -51,4 +49,3 @@ export const LoginPage = () => {
     </div>
   );
 };
-
