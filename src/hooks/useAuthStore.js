220383b6b27ae833ca4e8204e843dbfd048aa828
Cristian1805 <cristiancamilolozanogomez@gmail.com>
2023-10-24 16:00:53 -0500

    Login de usuario conectado con el backend

---
+++ src/hooks/useAuthStore.js
@@ -0,0 +1,25 @@
+import { useDispatch, useSelector } from 'react-redux';
+
+export const useAuthStore = () => {
+
+    const { status, user, errorMessage} = useSelector ( state => state.auth);
+    const dispatch = useDispatch();
+
+
+    const startLogin = async({email, password }) => {
+        console.log({ email, password});
+    }
+
+    return {
+
+        //* Propiedades
+        errorMessage,
+        status, 
+        user, 
+
+        //* Métodos
+        startLogin
+    }
+
+
+}
