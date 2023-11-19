50306d097322f1826358575bb6d839370a78bd7a
Cristian1805 <cristiancamilolozanogomez@gmail.com>
2023-11-19 11:31:16 -0500

    Realizada la paginacion en el componente de reportes

--- src/heroes/pages/Reportes.jsx
+++ src/heroes/pages/Reportes.jsx
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { getHeroById } from '../helpers';
+import { Table, Button, Pagination } from 'react-bootstrap'; // Importa los componentes necesarios de react-bootstrap 
 
 import jsPDF from 'jspdf';
 import 'jspdf-autotable'; 
 
 export const Reportes = () => {
   const [datos, setDatos] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [productsPerPage] = useState(7); // Número de productos por página 
 
   useEffect(() => {
     const url = 'http://localhost:5174' + '/inventario';
@@ -45,15 +48,25 @@ export const Reportes = () => {
     pdf.save('reporte.pdf');
   };
 
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = datos.slice(indexOfFirstProduct, indexOfLastProduct);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   return (
     <div className="container mt-4">
       <h1 className='form-title-importadas'> REPORTES FRUTY FENIX</h1>
       <hr className='form-divider-importadas' />
-      <button className="btn btn-primary mb-2" onClick={sortByDate}>Ordenar por Fecha</button>
-      <button className="btn btn-success mb-2 ml-2" onClick={exportToPDF}>Exportar a PDF</button>
+      <button className="btn btn-primary mb-2" onClick={sortByDate}>
+        Ordenar por Fecha
+      </button>
+      <button className="btn btn-success mb-2 ml-2" onClick={exportToPDF}>
+        Exportar a PDF
+      </button>
       <div className='table-responsive'>
-        <table id="miTabla" className="table table-striped table-bordered">
-          <thead className="thead-dark"> 
+        <Table striped bordered>
+          <thead className="thead-dark">
             <tr>
               <th>ID</th>
               <th>Nombre</th>
@@ -62,17 +75,26 @@ export const Reportes = () => {
             </tr>
           </thead>
           <tbody>
-            {datos.map(producto => {
-              const hero = getNombre(producto.id_producto);
-              return (<tr key={producto._id}>
-                <td>{producto.id_producto}</td>
-                <td>{hero.superhero}</td>
-                <td>{producto.calibre}</td>
-                <td>{producto.cantidad}</td>
-              </tr>);
+            {currentProducts.map((producto) => {
+              const hero = getNombre(producto.id_producto); 
+              return (
+                <tr key={producto._id}>
+                  <td>{producto.id_producto}</td>
+                  <td>{hero.superhero}</td>
+                  <td>{producto.calibre}</td>
+                  <td>{producto.cantidad}</td>
+                </tr>
+              );
             })}
           </tbody>
-        </table>
+        </Table>
+        <Pagination>
+          {[...Array(Math.ceil(datos.length / productsPerPage)).keys()].map((number) => (
+            <Pagination.Item key={number + 1} active={number + 1 === currentPage} onClick={() => paginate(number + 1)}>
+              {number + 1}
+            </Pagination.Item>
+          ))}
+        </Pagination>
       </div>
     </div>
   );
